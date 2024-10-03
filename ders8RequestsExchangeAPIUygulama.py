c5c9b68e34d4e4a2c39d86b2919ff90ba5a3a0e3
aksakalalper <aksakalalper@gmail.com>
2024-10-03 19:26:14 +0300

    Update ders8RequestsExchangeAPIUygulama.py

--- ders8RequestsExchangeAPIUygulama.py
+++ ders8RequestsExchangeAPIUygulama.py
@@ -5,8 +5,8 @@ import requests #web uzerinden veri cekilecegi icin requests modulu eklendi
 import json #geri donus .json formatinda olacagi icin json modulu eklendi.
 
 
-apiKey="397cb65fbf243cbae2b39c22" #exhangerate-api icin olusturulan uyeligin api-key bilgisi
-apiUrl="https://v6.exchangerate-api.com/v6/397cb65fbf243cbae2b39c22/latest/" #uyelige ozel api sorgu url'si
+apiKey="397cb65fbf24c22" #exhangerate-api icin olusturulan uyeligin api-key bilgisi
+apiUrl="httapi.com/v6/397cb65fbf243cbae2b39c22/latest/" #uyelige ozel api sorgu url'si
 
 while True:
     menuChoice=input('Select a choice:\n1-Convert money\n2-Exit\n')
